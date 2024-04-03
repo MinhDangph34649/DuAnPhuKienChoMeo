528663b0aa53b25591bdf561c11542e82297fd2c
MinhDangph34649 <dangnmph34649@fpt.edu.vn>
2024-04-03 18:48:48 +0700

    dang

--- duancho/app/src/main/java/com/example/duancho/MainActivity.java
+++ duancho/app/src/main/java/com/example/duancho/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private  String name ;
     private String hai;
+    private String min ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
