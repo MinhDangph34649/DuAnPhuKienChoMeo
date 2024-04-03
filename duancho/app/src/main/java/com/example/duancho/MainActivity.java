2ae7ba00d512a712cbe27e84eb39d26a46e49dbf
MinhDangph34649 <dangnmph34649@fpt.edu.vn>
2024-04-03 17:49:38 +0700

    lan1

--- duancho/app/src/main/java/com/example/duancho/MainActivity.java
+++ duancho/app/src/main/java/com/example/duancho/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private  String name ;
+    private String hai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
