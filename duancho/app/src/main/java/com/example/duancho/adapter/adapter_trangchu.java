f5d12f2c62e1de92bda5cbdbe03673641d5b1737
LucNguyen244 <Lucnguyen2442001@gmail.com>
2024-04-06 00:43:14 +0700

    Luc

--- duancho/app/src/main/java/com/example/duancho/adapter/adapter_trangchu.java
+++ duancho/app/src/main/java/com/example/duancho/adapter/adapter_trangchu.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 public class adapter_trangchu extends RecyclerView.Adapter<adapter_trangchu.ViewHo> {
     private ArrayList<SanPham> list;
     private Context context;
-    private String name ;
+
     SanPhamDao dao;
 
 
